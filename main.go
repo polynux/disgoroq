832ba5f2d87b59d29ef047e1a0ae8013c2d73ebf
Guillaume Dorce <polynux.dev@gmail.com>
2025-04-18 21:08:42 +0200

    improve embed

--- main.go
+++ main.go
@@ -533,8 +533,8 @@ func schedule(s *discordgo.Session) gocron.Scheduler {
 func sendFartingFriday(s *discordgo.Session) {
 	embed := &discordgo.MessageEmbed{
 		Title:       "🎉 FARTING FRIDAY NOTIFICATION 🎉",
-		Description: "Heeeeeeyyyyyy les amis du bruit de fond !!! 💨💨💨",
-		Color:       0x9B59B6,
+		Description: "@everyone Heeeeeeyyyyyy les amis du bruit de fond !!! 💨💨💨",
+		Color:       0x8B4513,
 		Fields: []*discordgo.MessageEmbedField{
 			{
 				Name:   "🌈✨ JOYEUX FARTING FRIDAY À TOUS LES PÉTOMANES EN HERBE ✨🌈",
@@ -561,6 +561,9 @@ func sendFartingFriday(s *discordgo.Session) {
 			Text: "*pffffrrrrrttttt* 💨 (c'était ma signature olfactive)",
 		},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: "https://media.discordapp.net/attachments/1194331990506356780/1362866990980796576/fartfireani.gif?ex=6803f44b&is=6802a2cb&hm=ecbbe318aa782a48b1ee9d1454b10870009bde37e96dcc470337a52d965271af&=",
+		},
+		Image: &discordgo.MessageEmbedImage{
 			URL: "https://media.discordapp.net/attachments/1194331990506356780/1362860968891384119/fartin.gif?ex=6803eeaf&is=68029d2f&hm=120759b2a6258432922d9e61239288ab3226dc925c86009e0402298c6b0e3df5&=",
 		},
 	}
@@ -579,7 +582,15 @@ func sendFartingFriday(s *discordgo.Session) {
 			log.Println("error getting farting friday channel,", err)
 			continue
 		}
-		_, err = s.ChannelMessageSendEmbed(channelID, embed)
+
+		_, err = s.ChannelMessageSendComplex(channelID, &discordgo.MessageSend{
+			Content: "🔊 **FARTING FRIDAY EST ARRIVÉ!** 💨 [Cliquez pour entendre le son légendaire](https://www.myinstants.com/en/instant/wet-fart-11093/)",
+			Embed:   embed,
+			AllowedMentions: &discordgo.MessageAllowedMentions{
+				Parse: []discordgo.AllowedMentionType{discordgo.AllowedMentionTypeEveryone},
+			},
+		})
+
 		if err != nil {
 			fmt.Println("error sending farting friday,", err)
 			continue
