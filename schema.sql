8be2f085a8580362db90cb5c8f8e6c196a451fa4
Guillaume Dorce <polynux.dev@gmail.com>
2025-12-14 17:11:54 +0100

    fix constraint in guild settings

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS guild_settings (
     id INTEGER PRIMARY KEY,
     guild_id TEXT NOT NULL,
     name TEXT NOT NULL,
-    value TEXT NOT NULL
+    value TEXT NOT NULL,
+    UNIQUE(guild_id, name)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_guild_settings_guild_id_name 
